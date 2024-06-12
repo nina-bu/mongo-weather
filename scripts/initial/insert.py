34ce1a95d4c193053a77c25cc95cd9817d937d31
nina <ninabu16@gmail.com>
2024-06-12 16:08:13 +0200

    feat: adjust weather data to 10 years ago

--- scripts/initial/insert.py
+++ scripts/initial/insert.py
@@ -25,17 +25,13 @@ countries_collection = db['countries']
 countries_collection.insert_many(countries_data_dict)
 print("Countries data inserted successfully")
 
-
 # Weather
 weather_file_path = '../../dataset/daily_weather.parquet'
-output_dir = 'dataset/json_output'
-os.makedirs(output_dir, exist_ok=True)
-
 weather_data = pd.read_parquet(weather_file_path)
-weather_data['date'] = pd.to_datetime(weather_data['date'])
-weather_data = weather_data[weather_data['date'].dt.year >= 2019]
-weather_data = weather_data.replace({np.nan: None})
 
+weather_data['date'] = pd.to_datetime(weather_data['date'])
+weather_data = weather_data[weather_data['date'].dt.year >= 2013]
+weather_data = weather_data.replace({np.nan: None})
 weather_data_dict = weather_data.to_dict('records')
 for record in weather_data_dict:
     record['date'] = record['date'].to_pydatetime()
